7b53d54b0f7c15e653faaca17ef86099970c824c
gsurjs <155509989+gsurjs@users.noreply.github.com>
2025-10-20 19:02:56 -0400

    starting folder screen class

---
+++ lib/folders_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'database_helper.dart';
+import 'cards_screen.dart';
+
+class FoldersScreen extends StatefulWidget {
+  @override
+  _FoldersScreenState createState() => _FoldersScreenState();
+}
